8154f0d89e362ab267501abfe8699a6e2c62ca4c
zizou707 <162337899+zizou707@users.noreply.github.com>
2024-06-26 17:58:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default function App() {
+
+  return (
+    <>
+      <div className="content">
+        
+      </div>
+    </>
+  );
+}
